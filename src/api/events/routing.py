1e6ec0d50d730874ed588e5aa28e01a1daa233a8
mutlu.python <mutlu.python@gmail.com>
2025-03-30 00:35:58 +0000

    Basic data types

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,9 +1,19 @@
 from fastapi import APIRouter
+from .schemas import EventSchema    
 
 router = APIRouter()
 
 @router.get("/")
 def read_events():
+    # list of the events
     return {
+        
         "results": [1,2,3]
     }
+
+
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+    # get the event by id
+    return {"id": event_id}
+    
